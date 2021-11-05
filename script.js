675861ba6478e0faceab4178d730916999752777
agajdosi <andreas.gajdosik@gmail.com>
2021-11-06 00:54:06 +0100

    Resize for mobile

--- script.js
+++ script.js
@@ -14,15 +14,26 @@ async function loadFrame() {
   var iframe = document.createElement('iframe');
   iframe.style.display = "none";
   iframe.className = "iframe";
-  iframe.src =iframe.src = "https://randomincategory.toolforge.org/Racism?site=en.wikipedia.org";
-  iframe.height = 600;
-  iframe.width = 800;
-  iframe.style.left = Math.floor(Math.random() * (window.innerWidth - width)) + 'px';
-  iframe.style.top = Math.floor(Math.random() * (window.innerHeight - height)) + 'px';
+  iframe.src = iframe.src = "https://randomincategory.toolforge.org/Racism?site=en.wikipedia.org";
+  iframe.height = height;
+  iframe.width = width;
+
+  //iframe.style.left = getRandomIntInclusive(0, window.innerWidth - width) + 'px';
+  //iframe.style.top = getRandomIntInclusive(0, window.innerHeight - height) + 'px';
+
 
   document.body.appendChild(iframe);
   await sleep(2000);
   iframe.style.display = "block";
-  
+
+  iframe.style.left = getRandomIntInclusive(-100, window.innerWidth-100) + 'px';
+  iframe.style.top = getRandomIntInclusive(-100, window.innerHeight-100) + 'px';
 
 }
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
