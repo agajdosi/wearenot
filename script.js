e037676b8671edc500cb3c6f8e33a4055de66c0c
agajdosi <andreas.gajdosik@gmail.com>
2021-11-06 01:50:26 +0100

    Finish the script

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var interval = setInterval(loadElement, 1000);
-
-function loadElement(){
-  loadFrame();
-}
+var interval = setInterval(loadFrame, 1000);
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -14,7 +10,9 @@ async function loadFrame() {
   var iframe = document.createElement('iframe');
   iframe.style.display = "none";
   iframe.className = "iframe";
-  iframe.src = iframe.src = "https://randomincategory.toolforge.org/Racism?site=en.wikipedia.org";
+
+  iframe.src = iframe.src = url;
+
   iframe.height = height;
   iframe.width = width;
 
